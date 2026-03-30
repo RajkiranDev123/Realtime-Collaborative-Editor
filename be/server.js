7fc66d4f259093703366b5cf229ef069879aab1c
RajkiranDev123 <rajtech645@gmail.com>
2026-03-30 07:04:52 +0530

    crdt

--- be/server.js
+++ be/server.js
@@ -5,17 +5,21 @@ import { YSocketIO } from "y-socket.io/dist/server";
 
 // Express is built only for HTTP request–response
 // WebSockets need connection upgrade, which Express doesn’t handle
-// Upgrade is needed to switch from one-time HTTP (request–response) to a 
+// Upgrade is needed to switch from one-time HTTP (request–response) to a
 // persistent WebSocket connection (real-time, bidirectional communication).
+
+// Express handles HTTP request–response only, but WebSocket needs an
+// “upgrade” — and only the raw HTTP server can handle that.
 
 const app = express();
 
-//HTTP server receives request & Passes it to Express
+// HTTP server receives request & Passes it to Express : httpServer.listen(port,cb)
 const httpServer = createServer(app);
 
-//now , Socket.IO is listening on SAME server
-//http://localhost:3000  → Express
-//ws://localhost:3000   → Socket.IO
+// now , Socket.IO is listening on SAME server
+// http://localhost:3000  → Express
+// ws://localhost:3000    → Socket.IO
+
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
@@ -23,8 +27,8 @@ const io = new Server(httpServer, {
   },
 });
 
-const ySocketIO = new YSocketIO(io);
-ySocketIO.initialize();
+const ySocketIO = new YSocketIO(io); // “Connect Yjs collaboration system to my Socket.IO server”
+ySocketIO.initialize(); // Starts syncing documents , Handles: updates , merging (CRDT)
 
 app.get("/", (req, res) => {
   res.status(200).json({
